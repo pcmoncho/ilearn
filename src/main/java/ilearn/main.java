bf6452e5053cf456816a1718d701876fe07cd03e
Moncho Chao <mchao@pcmoncho.cl>
2017-09-04 01:16:29 -0300

    first commit

---
+++ src/main/java/ilearn/main.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package ilearn;
+
+import java.util.Random;
+
+/**
+ * @author pcmon
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Proposito
+		//Operaciones
+		//Memorizar
+		//Probar
+		//Aprender
+		//Pregunta al otro yo
+		
+		Random ram = new Random();
+		int nextNumbre = ram.nextInt();		
+		int memorySize =10;
+		
+		boolean[] memory = new boolean [memorySize];
+
+		
+
+	}
+
+}
